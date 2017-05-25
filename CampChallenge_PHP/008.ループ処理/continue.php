9e464532d04bcf71ecab158bf9a0c2487ab5e68a
DESKTOP-F8PL7HS\Amber <k.saegusa11@gmail.com>
2017-05-25 16:33:36 +0900

    【PHP課題】　ループ処理　continueとbreak

---
+++ CampChallenge_PHP/008.ループ処理/continue.php
@@ -0,0 +1,16 @@
+﻿<?php
+$a = array(1,2,3,4,5,6,7,8,9);
+$b = array(1,2,3,4,5,6,7,8,9);
+$total = 1;
+
+foreach($a as $value1){
+  if($value1 == 5){
+    echo '【5の段はスキップします】'."<br>";
+    continue;   
+  }
+  foreach($b as $value2){
+    $total = $value1 * $value2;
+    echo $value1.'×'.$value2.'='.$total."<br>";
+  }
+}
+?>
---
+++ CampChallenge_PHP/008.ループ処理/continueとbreak.php
@@ -0,0 +1,19 @@
+﻿<?php
+$a = array(1,2,3,4,5,6,7,8,9);
+$b = array(1,2,3,4,5,6,7,8,9);
+$total = 1;
+
+foreach($a as $value1){
+  if($value1 == 5){
+    echo '【5の段はスキップします】'."<br>";
+    continue;
+  }elseif($value1 == 8){
+    echo '【7の段までで終了します】'."<br>";
+    break;    
+  }
+  foreach($b as $value2){
+    $total = $value1 * $value2;
+    echo $value1.'×'.$value2.'='.$total."<br>";
+  }
+}
+?>
