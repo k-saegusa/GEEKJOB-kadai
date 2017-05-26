d91ba49b1dd1420cb1b19c0bb79a7f117aa20aad
DESKTOP-F8PL7HS\Amber <k.saegusa11@gmail.com>
2017-05-26 15:16:43 +0900

    【PHP課題】　関数　引数1,2

---
+++ CampChallenge_PHP/009.関数/引数1.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+function suuti($a){
+if($a % 2 == 0){
+
+echo $a.'は偶数です。'."<br>";
+
+}else{
+
+echo $a.'は奇数です。'."<br>";
+
+}
+
+}
+
+
+suuti("2");
+
+suuti("5");
+
+suuti("69");
+
+suuti("256");
+
+?>
---
+++ CampChallenge_PHP/009.関数/引数2.php
@@ -0,0 +1,14 @@
+﻿<?php
+function kakezan($a,$b = 5,$c = false){
+      $d = $a * $b;
+      if($c == true){
+        $d = $d * $d;
+        echo $a.'×'.$b.'の2乗は',$d.'です。'."<br>";
+      }elseif($c == false){
+        echo $a.'×'.$b.'は',$d.'です。'."<br>";
+      }
+    }
+
+kakezan(3,4,true);
+kakezan(2);
+ ?>
