b323a051471a6b286965f950cd04aaf848c23ef7
DESKTOP-F8PL7HS\Amber <k.saegusa11@gmail.com>
2017-05-28 14:29:34 +0900

    【PHP課題】　関数　戻り値

---
+++ CampChallenge_PHP/009.関数/戻り値1.php
@@ -0,0 +1,15 @@
+﻿<?php
+function myprofile(){
+  echo '名前 : 三枝 佳子'."<br>".
+  '生年月日 : 1995/3/29'."<br>".
+  '自己紹介 : よろしくお願いします。'."<br>";
+//  echo myprofile();
+  return true;
+}
+$ans = myprofile();
+if($ans == true){
+  echo 'この処理は正しく実行できました'."<br>";
+}else{
+  echo '正しく実行できませんでした'."<br>";
+}
+?>
---
+++ CampChallenge_PHP/009.関数/戻り値2.php
@@ -0,0 +1,14 @@
+﻿<?php
+function prof(){
+  $a = array();
+  $a['id'] = '000';
+  $a['na'] = '三枝 佳子';
+  $a['bi'] = '1995/3/29';
+  $a['ad'] = '東京都';
+  return $a;
+}
+$profile = prof();
+echo $profile['na']."<br>".
+$profile['bi']."<br>".
+$profile['ad']."<br>";
+?>
