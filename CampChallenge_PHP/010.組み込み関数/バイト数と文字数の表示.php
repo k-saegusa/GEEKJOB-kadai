8ec4e04a2c72ae1db52c24a9e401e55acc1d0e4c
DESKTOP-F8PL7HS\Amber <k.saegusa11@gmail.com>
2017-06-02 20:50:37 +0900

    【PHP課題】　組み込み関数

---
+++ CampChallenge_PHP/010.組み込み関数/バイト数と文字数の表示.php
@@ -0,0 +1,6 @@
+﻿<?php
+$name1 = strlen('三枝佳子');
+$name2 = mb_strlen('三枝佳子');
+
+echo '私の名前は'.$name1.'バイトで'.$name2.'文字です。';
+ ?>
---
+++ CampChallenge_PHP/010.組み込み関数/文字の入れ替え.php
@@ -0,0 +1,8 @@
+﻿<?php
+$bunsyo = '「きょUはぴIえIちぴIの
+くみこみかんすUのがくしゅUをしてIます」';
+$taisyo = array('U','I');
+$irekae = array('う','い');
+
+echo str_replace($taisyo,$irekae,$bunsyo);
+ ?>
