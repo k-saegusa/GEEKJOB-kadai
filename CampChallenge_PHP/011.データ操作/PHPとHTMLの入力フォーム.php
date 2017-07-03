16bf781014975d490c83e87756398be8ac8e943f
DESKTOP-F8PL7HS\Amber <k.saegusa11@gmail.com>
2017-07-03 18:12:59 +0900

    【PHP課題】データ操作

---
+++ CampChallenge_PHP/011.データ操作/PHPとHTMLの入力フォーム.php
@@ -0,0 +1,24 @@
+﻿<html>
+  <head>
+    <meta charset="utf-8">
+    <title>テスト</title>
+  </head>
+  <body>
+    <form action="TEST.php" method="post">
+      <b>■名前を入力してください<br><b>
+        <input type="text" name="text" value="<?=$_POST["text"]?>">
+        <br>
+      <b>■性別を入力してください<br><b>
+        <?php ($_POST["radio"]=="男")?$val="checked":$val=""; ?>
+        <input type="radio" name="radio" value="男"<?=$val?>>男
+        <?php ($_POST["radio"]=="女")?$val="checked":$val=""; ?>
+        <input type="radio" name="radio" value="女"<?=$val?>>女
+        <br>
+      <b>■趣味を入力してください<br><b>
+      <textarea rows="8" cols="80" name='textarea'><?=$_POST['textarea']?>
+      </textarea>
+      <br>
+      <input type="submit" name="btnSubmit"><br>
+    </form>
+  </body>
+</html>
---
+++ CampChallenge_PHP/011.データ操作/セッションの記録と表示.php
@@ -0,0 +1,8 @@
+﻿<?php
+session_start();
+date_default_timezone_set('Asia/Tokyo');
+$a =  date('Y/m/d h:i:s');
+
+$_SESSION['zenkailogin'] = $a;
+echo '前回記録した日時は'.$_SESSION['zenkailogin'];
+?>
