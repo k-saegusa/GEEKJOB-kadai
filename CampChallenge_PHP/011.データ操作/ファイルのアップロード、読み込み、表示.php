86080c4174033a054021c31a5676959022227fa0
DESKTOP-F8PL7HS\Amber <k.saegusa11@gmail.com>
2017-07-08 22:07:56 +0900

    【PHP課題】　データ操作　ファイルアップロード

---
+++ CampChallenge_PHP/011.データ操作/ファイルのアップロード、読み込み、表示.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>テスト</title>
+  </head>
+  <body>
+    <form enctype="multipart/form-data" action="TEST.php" method="post">
+      <input type="file" name="file">
+      <input type="submit" value="送信">
+<?php
+# var_dump($_FILES['file']);
+$a='./img/'.$_FILES['file']['name'];
+$a=mb_convert_encoding($a,"cp932","utf8");
+if(move_uploaded_file($_FILES['file']['tmp_name'],$a)){
+  echo file_get_contents($a);
+}
+?>
+  </form>
+  </body>
+</html>
