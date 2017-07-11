5ee0fdd6b8258a1df7cba9bee6347230c128975c
DESKTOP-F8PL7HS\Amber <k.saegusa11@gmail.com>
2017-07-11 22:00:14 +0900

    【PHP課題】　データ操作　クエリストリング

---
+++ CampChallenge_PHP/011.データ操作/クエリストリング(簡易素因数分解).php
@@ -0,0 +1,32 @@
+﻿<html>
+  <head>
+    <meta charset="utf-8">
+    <title>テスト</title>
+  </head>
+  <body>
+    <form action="TEST.php" method="GET">
+      <b>■値を入力してください<br><b>
+      <input type="text" name='atai'>
+      <br>
+      <input type="submit" name="btnSubmit" value="送信"><br>
+    </form>
+  </body>
+</html>
+
+<?php
+foreach($_GET as $name => $value){
+  $$name = $value;
+}
+
+$a= array(2,3,5,7);
+
+echo '元の値：'.$atai."<br>".'一桁の素因数：';
+
+foreach($a as $value) {
+  while ($atai%$value==0){
+    $atai=$atai/$value;
+  }
+  echo $value."\n";
+}
+echo "<br>".'余った値：'.$atai;
+?>
